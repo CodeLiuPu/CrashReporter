278a8130a4ece7328aa1036c898cd9eebd51423c
liupu <575866447@qq.com>
2019-08-25 21:37:43 +0800

    FileUtils 优化

--- crashreporter/src/main/java/com/update/crashreporter/utils/FileUtlis.java
+++ crashreporter/src/main/java/com/update/crashreporter/utils/FileUtlis.java
@@ -1,8 +1,12 @@
 package com.update.crashreporter.utils;
 
-import android.util.Log;
+import android.Manifest;
+import android.os.Build;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 /**
  * @author : liupu
@@ -19,6 +23,36 @@ public class FileUtlis {
     private FileUtlis() {
     }
 
-    public static void saveEx2File() {
+    /**
+     * 向文件中写入String数据
+     */
+    public static void saveString2File(String content) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (PermissionUtils.checkPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                filePath = GlobalContext.getApp().getExternalFilesDir("Crash").getAbsolutePath() + File.separator + fileName;
+            }
+        }
+
+        // 创建文件
+        File file = new File(filePath);
+        if (!file.exists()) {
+            File dir = new File(file.getParent());
+            boolean mkdirs = dir.mkdirs();
+            try {
+                boolean newFile = file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // 向文件中写数据
+        if (file.exists()) {
+            try (FileOutputStream os = new FileOutputStream(file)) {
+                os.write(content.getBytes());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
     }
 }
